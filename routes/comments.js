01e861900dbf23bdf77ad09e42ee432eb0e405cb
jlounsb1 <jefflounsbury@gmail.com>
2024-07-10 21:35:39 -0400

    adds a route to get a comment by Id

--- routes/comments.js
+++ routes/comments.js
@@ -11,5 +11,23 @@ router
 .get((req, res) =>{
     res.json(comments)
 })
+.post((req, res, )=>{
+    const comment = {
+        id: comments[comments.length - 1].id + 1,
+        userId: req.body.userId,
+        postId: req.body.postId,
+        body: req.body.body,
+      };
+      comments.push(comment);
+      res.json(comments[comments.length - 1]);
+})
+
+router.route("/:id")
+.get((req, res, next) => {
+    const comment = (comments.find((p) => p.id == req.params.id))
+    if (comment) res.json({comment});
+        else
+        next();
+})
 
 module.exports = router;
